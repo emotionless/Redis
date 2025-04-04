3f62502756ef931b6354b0bf8e50d5f49252549a
Faruk Hossain <miloniitju@gmail.com>
2025-04-04 23:03:34 +0800

    Restore files

--- rbay/rbay/src/services/keys.ts
+++
@@ -1,4 +0,0 @@
-export const pageCacheKey = (id: string) => `pagecache#${id}`;
-export const usersKey = (id: string) => 'users#' + id;
-export const sessionsKey = (id: string) => `sessions#${id}`;
-export const itemsKey =  (id: string) => `items#${id}`;
