ab66210c51b9d978f29ff5e1a264400e348ec3e4
Faruk Hossain <miloniitju@gmail.com>
2025-04-20 22:42:21 +0800

    Showing Common Liked Items

--- rbay/src/services/queries/likes.ts
+++ rbay/src/services/queries/likes.ts
@@ -28,4 +28,8 @@ export const unlikeItem = async (itemId: string, userId: string) => {
     }
 };
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+    const commonIds = await client.sInter([userLikesKey(userOneId), userLikesKey(userTwoId)]);
+
+    return getItems(commonIds);
+};
