55e346a9ef2e15c3ec69885b4450950b60713fd3
Faruk Hossain <miloniitju@gmail.com>
2025-07-28 23:36:12 +0800

    Solving Multi Bids Concurrency Issue with a Transaction

--- rbay/src/services/queries/bids.ts
+++ rbay/src/services/queries/bids.ts
@@ -5,25 +5,30 @@ import { DateTime } from 'luxon';
 import { getItem } from './items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-	const item = await getItem(attrs.itemId);
-	if (!item) {
-		throw new Error(`Item with ID ${attrs.itemId} does not exist.`);
-	}
-	if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
-		throw new Error(`Item with ID ${attrs.itemId} has already ended.`);
-	}
-	if (attrs.amount <= item.price) {
-		throw new Error(`Bid amount must be greater than the current price of the item.`);
-	}
-	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
-	Promise.all([
-		client.rPush(bidHistoryKey(attrs.itemId), serialized),
-		client.hSet(itemsKey(item.id), {
-			price: attrs.amount,
-			highestBidUserId: attrs.userId,
-			bids: item.bids + 1
-		}),
-	]);
+
+	return client.executeIsolated(async (isolatedClient) => {
+		await isolatedClient.watch(itemsKey(attrs.itemId));
+		const item = await getItem(attrs.itemId);
+		if (!item) {
+			throw new Error(`Item with ID ${attrs.itemId} does not exist.`);
+		}
+		if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+			throw new Error(`Item with ID ${attrs.itemId} has already ended.`);
+		}
+		if (attrs.amount <= item.price) {
+			throw new Error(`Bid amount must be greater than the current price of the item.`);
+		}
+		const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+		return isolatedClient
+			.multi()
+			.rPush(bidHistoryKey(attrs.itemId), serialized)
+			.hSet(itemsKey(item.id), {
+				price: attrs.amount,
+				highestBidUserId: attrs.userId,
+				bids: item.bids + 1
+			})
+		.exec()
+	});
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
